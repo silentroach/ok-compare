7ddfd18e8c61efb2651c3d53c89aa5ddf6a691b6
Igor Kalashnikov <igor.kalashnikov@me.com>
2026-05-02 21:14:44 +0300

    test

--- apps/www/vitest.config.ts
+++ apps/www/vitest.config.ts
@@ -5,5 +5,7 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
   test: {
     environment: 'node',
+    include: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
+    exclude: ['tests/**/*.visual.local.spec.ts'],
   },
 });
